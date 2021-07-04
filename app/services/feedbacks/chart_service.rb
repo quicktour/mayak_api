d54b3604575a3d0630f3543c6b85f2fa640ba08b
Viktor Kuras <kurasviktor@gmail.com>
2021-07-05 01:04:45 +0300

    Add chert service and improve chart service spec

--- app/services/feedbacks/chart_service.rb
+++ app/services/feedbacks/chart_service.rb
@@ -1,4 +1,36 @@
 class Feedbacks::ChartService < ApplicationService
+  def initialize(card_id, range)
+    @card_id = card_id
+    @range = range
+  end
+
   def call
+    response = Feedbacks::ListService.call(@card_id)
+    cutted_result = RangeCutterService.call(response.body['feedbacks'], @range)
+    build_cart(cutted_result)
+  end
+
+  private
+
+  def build_cart(cutted_result)
+    pre_result = {}
+    cutted_result.each do |item|
+      key = DateTime.parse(item['createdDate']).strftime("%b %d")
+
+      if pre_result.has_key?(key)
+        pre_result[key] += 1
+      else
+        pre_result[key] = 1
+      end
+    end
+
+    result_template(pre_result.keys.map(&:to_s), pre_result.values.map(&:to_s))
+  end
+
+  def result_template(labels, data)
+    {
+      labels: labels,
+      data: data
+    }
   end
 end
--- spec/services/feedbacks/chart_service_spec.rb
+++ spec/services/feedbacks/chart_service_spec.rb
@@ -12,37 +12,17 @@ RSpec.describe Feedbacks::ChartService, 'call' do
     end    
 
     it 'test valid keys in return data' do
-      expect(result.last.has_key?('labels')).to be true
-      expect(result.last.has_key?('data')).to be true
+      expect(@result.has_key?(:labels)).to be true
+      expect(@result.has_key?(:data)).to be true
     end
 
     it 'test return data has valid kind in keys' do
-      expect(result['labels']).to be_a_kind_of(Array)
-      expect(result['data']).to be_a_kind_of(Array)
+      expect(@result[:labels]).to be_a_kind_of(Array)
+      expect(@result[:data]).to be_a_kind_of(Array)
     end
 
     it 'test return data has same count in keys' do
-      expect(result['labels'].count).to eq(result['data'].count)
-    end
-  end
-  
-  describe 'it returns card feedbacks chart data in range' do
-    before do
-      @card = create(:card)
-    end
-
-    it 'test return data in week range' do
-      result = Feedbacks::ChartService.call(@card.imt_id, :week)
-      week_range = (Date.today..1.week.ago.to_date).map{ |date| date.strftime("%b %d") }
-
-      expect(result['labels']).to eq(week_range)
-    end
-
-    it 'test return data in month range' do
-      result = Feedbacks::ChartService.call(@card.imt_id, :month)
-      month_range = (Date.today..1.month.ago.to_date).map{ |date| date.strftime("%b %d") }
-
-      expect(result['labels']).to eq(month_range)
+      expect(@result[:labels].count).to eq(@result[:data].count)
     end
   end
 end
